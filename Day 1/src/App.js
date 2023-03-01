23472e9c8478e64b7eae4f3394492487c0255e91
Krish Patel <85817172+Krish2804@users.noreply.github.com>
2023-03-02 04:38:31 +0530

    Add files via upload

---
+++ Day 1/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+export const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <h2>This is Day 1 project.</h2>
+    </div>
+  );
+}
+
+// export default App;
+// export {App};
